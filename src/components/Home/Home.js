ac995f04ad40a47a439bcdb53ea017eafc6a986d
lukson33 <45272632+lukson33@users.noreply.github.com>
2021-04-22 02:44:40 +0200

    filter options added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { DataContext } from "../../context/context";
 
 const Home = () => {
   const [data, setData] = useContext(DataContext);
+  const [invoices, setInvoices] = useState(data);
+  const [filter, setFilter] = useState("all");
+
+  const filterData = (status) => {
+    setFilter(status);
+  };
+
+  useEffect(() => {
+    // if (filter === "paid") {
+    //   const filteredData = data.filter((invoice) => invoice.status === "paid");
+    //   setInvoices(filteredData);
+    // } else if (filter === "pending") {
+    //   const filteredData = data.filter(
+    //     (invoice) => invoice.status === "pending"
+    //   );
+    //   setInvoices(filteredData);
+    // }
+    returnFiltered(filter);
+  }, [filter]);
+
+  const returnFiltered = (status) => {
+    if (status === "all") {
+      setInvoices(data);
+    } else {
+      const filteredData = data.filter((invoice) => invoice.status === status);
+      setInvoices(filteredData);
+    }
+  };
+
   return (
     <div>
-      {data.map((invoice) => (
+      <button onClick={() => filterData("all")}>All Invoices</button>
+      <button onClick={() => filterData("paid")}>Paid</button>
+      <button onClick={() => filterData("pending")}>Pending</button>
+      <button onClick={() => filterData("draft")}>Draft</button>
+
+      {invoices.map((invoice) => (
         <Link
           to={{
             pathname: `/invoice/${invoice.id}`,
